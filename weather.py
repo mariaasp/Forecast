49921e9c609d624a8bca9ff77c77ac927df729dd
mariaasp <mariaasp@stud.ntnu.no>
2017-04-02 02:15:19 +0200

    some minor syntax changes

--- weather.py
+++ weather.py
@@ -1,53 +1,69 @@
 import requests
 import xml.etree.ElementTree as ET 
 
+DEBUG = 0
 
 
 def get_nowcast_for_location(latitude, longditude):  
 
 	parameters = {"lat": latitude, "lon": longditude} 
+
+	# Read XML data from URL
 	response = requests.get("http://api.met.no/weatherapi/nowcast/0.9/", params=parameters)
 
-	# print(response.status_code) #prints status (200 - OK)
+	if DEBUG:
+		print(response.status_code) # (200 - OK)
 
-	root = ET.fromstring(response.content) # root element in xml tree
+	# Root element in xml tree
+	root = ET.fromstring(response.content) 
 
 	return root
 
 
 def get_max_precipitation(root):
 
-	product = root.find('product')
-	times = product.findall('time')
+	unit 			  = None
 	max_precipitation = 0
-	unit = None
 
-	for time in times:
-#		print time.tag, time.attrib
+	# Traverse the xml tree
+	# Product - the node in the xml tree that contains the nowcast data at continuous time steps with 8 minutes intervals
+	product = root.find('product')
+	
+	time_steps = product.findall('time')
+
+	for time_step in time_steps:
+
+		if DEBUG:
+			print time_step.tag, time_step.attrib
 		
-		locations = time.findall('location')
+		locations = time_step.findall('location')
+
 		for location in locations:
 			precipitation = location.find('precipitation')
-			value = float(precipitation.attrib['value'])
-			unit = precipitation.attrib['unit']
 
-			if value > max_precipitation:
-				max_precipitation = value
+			unit  		  = precipitation.get('unit')
+			precipitation = float(precipitation.get('value'))
+
+			if precipitation > max_precipitation:
+				max_precipitation = precipitation
 
 	return max_precipitation, unit
 
 
 
 def need_umbrella(precipitation, threshold=1):
+
 	if precipitation >= threshold:
 		return True
+	
 	return False
 
 
-root = get_nowcast_for_location(63.42, 10.36) # location for Ila, Trondheim : lat: 63.42, long: 10.36
+root = get_nowcast_for_location(63.42, 10.36) # location for Ila, Trondheim
 max_precipitation, unit = get_max_precipitation(root)
 
 print "Maximum precipitation expected for the next 2 hours:", max_precipitation, unit 
+
 if need_umbrella(max_precipitation, threshold=0.5):
 	print "You need to bring an umbrella!"
 else:
